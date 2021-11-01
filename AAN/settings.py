93355302628c38324328ca013bf9df39697eaa68
stronginasuit <76620622+stronginasuit@users.noreply.github.com>
2021-11-01 17:57:20 +0100

    Update settings.py

--- AAN/settings.py
+++ AAN/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a3=drc(r7huay+)862prjrl0nwu7&9g#!5tjdn)-kef!6)%b&#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
